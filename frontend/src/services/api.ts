2ca32ea4dd8c628c8386d70cf89a4f2a087ad78c
Manprabesh Boruah <billionaireboruah31@gmail.com>
2026-03-23 17:16:39 +0530

    updated api for

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -22,13 +22,31 @@ export const login = async (email: string, password: string) => {
   return res.json();
 };
 
-export const message = async(prompt:string)=>{
-     const res = await fetch(`${API_URL}/message`, {
+export const message = async (prompt: string) => {
+  const res = await fetch(`${API_URL}/ask-ai`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({prompt }),
+    body: JSON.stringify({ prompt }),
     credentials: "include",
   });
 
   return res.json();
+}
+export const saveMessage = async (prompt: string, response: string) => {
+  const res = await fetch(`${API_URL}/save-message`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ prompt, response }),
+    credentials: "include",
+  });
+
+  return res.json();
+}
+
+export const getMessage = async (page:number) => {
+  const res = await fetch(`http://localhost:5000/api/v1/get-message?page=${page}`, {
+    credentials: "include",
+  });
+
+    return res.json();
 }
