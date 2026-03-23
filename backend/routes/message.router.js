63745ad98d21d8ca4144b7a2be2cdd23af343377
Manprabesh Boruah <billionaireboruah31@gmail.com>
2026-03-23 17:21:33 +0530

    updated route folder

--- backend/routes/message.router.js
+++ backend/routes/message.router.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { createMessage } from "../controllers/message.controller.js";
-
+import { createMessage, saveMessage, getMessage } from "../controllers/message.controller.js";
+import authenticate from "../middleware/auth.js";
 const messageRouter = express.Router();
 
-messageRouter.post("/message", createMessage);
+messageRouter.post("/ask-ai", authenticate, createMessage);
+messageRouter.post("/save-message", authenticate, saveMessage);
+messageRouter.get("/get-message", authenticate, getMessage);
 
 export default messageRouter;
