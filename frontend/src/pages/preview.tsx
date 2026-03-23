c8080b6b2730b211b2a0d4985d434483df5a8678
Manprabesh Boruah <billionaireboruah31@gmail.com>
2026-03-23 17:15:45 +0530

    added preview page for displaying the responses

--- frontend/src/pages/preview.tsx
+++ frontend/src/pages/preview.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import "../stylesheet/storePreview.css"
+import { getMessage } from "../services/api";
 type Message = [{
     prompt: string;
     response: string;
@@ -13,18 +14,14 @@ const StorePreview = () => {
         try {
 
 
-            const res = await fetch(`http://localhost:5000/api/v1/get-message?page=${page}`, {
-                credentials: "include",
-            });
-
-            const result = await res.json();
+            const result = await getMessage(page);
             if (result.success) {
 
                 setData((prev) => [...prev, ...result.data.message]);
             }
 
             else {
-                console.log("failes response", res)
+                console.log("failes response")
             }
         } catch (err) {
             console.error("Fetch error:", err);
@@ -53,11 +50,11 @@ const StorePreview = () => {
                     ))
                 ) : (
                     <div className="no-item">
-                    <div key="no card" className="no-item">
-                        <h3 className="card-title">No Message</h3>
-                        {/* <div className="card-divider" /> */}
-                        <p className="card-text">Nothing here yet. Messages will appear once they arrive.</p>
-                    </div>
+                        <div key="no card" className="no-item">
+                            <h3 className="card-title">No Message</h3>
+                            {/* <div className="card-divider" /> */}
+                            <p className="card-text">Nothing here yet. Messages will appear once they arrive.</p>
+                        </div>
                     </div>
                 )}
                 {/* <Loader message="no message"/> */}
