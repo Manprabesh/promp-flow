45b9e385b66b7ae8bcfb1225cb39ccda7504a6e5
Manprabesh Boruah <billionaireboruah31@gmail.com>
2026-03-24 00:00:49 +0530

    added frontend uri

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: process.env.FRONTEND_URI, 
   credentials: true,               
 }));
 app.use(cookieParser())
@@ -26,5 +26,5 @@ app.use("/api/v1",messageRouter);
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port${PORT}`);
 });
