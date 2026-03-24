5d4d4e011cf2cb07afee23ebea7e7a80b2a86be4
Manprabesh Boruah <billionaireboruah31@gmail.com>
2026-03-24 19:51:21 +0530

    added loader

--- frontend/src/pages/signup.tsx
+++ frontend/src/pages/signup.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { signup } from "../services/api";
 import "../stylesheet/auth.css";
 import { Link, useNavigate } from "react-router-dom";
+import Loader from "../components/Loader";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
@@ -32,45 +33,50 @@ const Signup = () => {
   };
 
   return (
-    <div className="auth-container">
-      <div className="auth-card">
-        <h2 className="auth-title">Signin</h2>
+    <>
+      {
+        loader && <Loader />
+      }
+      <div className="auth-container">
+        <div className="auth-card">
+          <h2 className="auth-title">Signin</h2>
 
-        <form className="auth-form" onSubmit={handleSubmit}>
-          <input
-            className="auth-input"
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+          <form className="auth-form" onSubmit={handleSubmit}>
+            <input
+              className="auth-input"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
 
-          <input
-            className="auth-input"
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+            <input
+              className="auth-input"
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
 
-          <input
-            className="auth-input"
-            type="password"
-            placeholder="confirm password"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
+            <input
+              className="auth-input"
+              type="password"
+              placeholder="confirm password"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
 
-          <button className="auth-button" type="submit">
-            Signup
-          </button>
-        </form>
+            <button className="auth-button" type="submit">
+              Signup
+            </button>
+          </form>
 
-        <p className="auth-link">
-          Don't have account? <Link to="/signup">Login</Link>
-        </p>
+          <p className="auth-link">
+            Don't have account? <Link to="/signup">Login</Link>
+          </p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
