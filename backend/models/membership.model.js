82fcef07157ad2129f2ffe360906451dde0ec767
Manprabesh Boruah <billionaireboruah31@gmail.com>
2026-03-31 05:57:48 +0530

    keep track of the user and groups

---
+++ backend/models/membership.model.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+const membershipSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    group: {
+        type: Schema.Types.ObjectId,
+        ref: "Group",
+        required: true,
+    },
+    role: {
+        type: String,
+        enum:["admin","member"],
+        trim:true,
+        required: true
+    },
+    joinedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+membershipSchema.index({ user: 1, group: 1 }, { unique: true });
+membershipSchema.index({ group: 1 });
+export default model("Membership", membershipSchema);
