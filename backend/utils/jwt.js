aa6c10f06079cec4438e8b4fee0451ebf6f270fc
Manprabesh Boruah <billionaireboruah31@gmail.com>
2026-04-01 03:59:56 +0530

    .

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken"
-export default function generateToken(payload) {
+export default function generateToken(payload,) {
   return jwt.sign(
     payload,
     process.env.JWT_SECRET,
