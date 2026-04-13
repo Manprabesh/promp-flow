66e72d7ded2d429ea5d408d24744e7d69af08254
Manprabesh Boruah <billionaireboruah31@gmail.com>
2026-04-13 15:23:09 +0530

    updater routes

--- backend/routes/auth.router.js
+++ backend/routes/auth.router.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { signUp,login } from "../controllers/auth.controller.js";
-
+import { signUp,login,OAuthLogin,checkAuth } from "../controllers/auth.controller.js";
+import authenticate from "../middleware/auth.js";
 const authRouter = express.Router();
 
 authRouter.post("/signup", signUp);
 authRouter.post("/login", login);
+authRouter.post("/oauth", OAuthLogin);
+authRouter.get("/check-auth",authenticate,checkAuth)
 
 export default authRouter;
--- backend/routes/group.router.js
+++ backend/routes/group.router.js
@@ -1,12 +1,13 @@
 import express from "express"
 import authenticate from "../middleware/auth.js"
 import linkValidator from "../middleware/linkValidator.js"
-import { createGroup, generateInvitationLink, joinGroup, displayGroups } from "../controllers/group.controller.js";
+import { createGroup, generateInvitationLink, joinGroup, displayGroups,verifyRoom } from "../controllers/group.controller.js";
 const groupRouter = express.Router();
 
 groupRouter.post("/create-group",authenticate,createGroup);
 groupRouter.get("/generate-link/:groupName",authenticate,generateInvitationLink);
 groupRouter.get("/join-group/:link",authenticate,linkValidator,joinGroup);
 groupRouter.get("/display-group",authenticate,displayGroups);
+groupRouter.get("/verify-link/:link",authenticate,verifyRoom);
 
 export default groupRouter;
